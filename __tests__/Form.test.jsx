22192c453d4001592813f01429f19a803ca00346
Emilio Bielanski <Emilio.Bielanski@outlook.com>
2023-10-10 11:58:21 +0200

    new component and integration test

---
+++ __tests__/Form.test.jsx
@@ -0,0 +1,23 @@
+/**
+ * @group component
+ */
+
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Form from '../src/components/Form';
+
+
+describe('Form Component', () => {
+  it('renders without crashing', () => {
+    const { getByPlaceholderText } = render(<Form onChange={() => {}} onFilter={() => {}} />);
+    expect(getByPlaceholderText('Search')).toBeInTheDocument();
+  });
+});
+
+
+
+
+
